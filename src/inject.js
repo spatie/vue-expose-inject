61f94c657c09dd7226759595ff433911a7cec445
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-11-23 14:41:09 +0100

    CS

--- src/inject.js
+++ src/inject.js
@@ -1,24 +1,24 @@
 import { normalizeMap } from './util';
 
-function resolveDependency(key, vm) {
+function resolveExposedProperty(key, vm) {
 
     if (key in (vm.$exposed || {})) {
         return vm.$exposed[key];
     }
 
     if (vm.$options.parent) {
-        return resolveDependency(key, vm.$options.parent);
+        return resolveExposedProperty(key, vm.$options.parent);
     }
 
     throw new Error(`Dependency \`${key}\` couldn't be resolved`);
 }
 
 function inject(keys) {
-    return normalizeMap(keys).reduce((properties, { key, property }) => {
-        properties[key] = function () {
-            return resolveDependency(property, this);
+    return normalizeMap(keys).reduce((resolvedProperties, { key, property }) => {
+        resolvedProperties[key] = function () {
+            return resolveExposedProperty(property, this);
         };
-        return properties; 
+        return resolvedProperties; 
     }, {});
 }
 
